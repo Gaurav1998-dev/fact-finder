72140892cad890a89db95f54f11c2de59f026475
Gaurav Varma <gaurav@inkeep.com>
2025-08-02 12:40:58 -0700

    Add next-app source files to the project

---
+++ next-app/src/components/copilotkit-component.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { CopilotChat } from "@copilotkit/react-ui";
+import { CopilotKit } from "@copilotkit/react-core";
+import "@copilotkit/react-ui/styles.css";
+ 
+export function CopilotKitComponent({ runtimeUrl }: { runtimeUrl: string}) {
+  return (                                                     
+    <CopilotKit
+      runtimeUrl={runtimeUrl}
+      agent="weatherAgent"
+    >
+      <CopilotChat
+        labels={{
+          title: "Your Assistant",
+          initial: "Hi! 👋 How can I assist you today?",
+        }}
+      />
+    </CopilotKit>
+  );
+}
